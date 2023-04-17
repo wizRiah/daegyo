d7ecdd25ef8fb827baf0fae250a713b57baac12a
wizRiah <jrjuria@nate.com>
2023-04-17 17:36:34 +0900

    Update applications.vue

--- quasar-project/src/pages/teacher/mypage/applications.vue
+++ quasar-project/src/pages/teacher/mypage/applications.vue
@@ -8,11 +8,21 @@
       <li class="material-icons active">홈</li>
     </ul>
 
-    <!-- 시작 -->
-    <div>04_지원현황</div>
+
+    <!-- 타이틀 -->
+    <div class="my_txtbox">
+      <strong class="text-h1">지원현황</strong>
+      <ul class="text-subtitle4">
+        <li>최근 1년 이내의 지원내역을 확인하실 수 있습니다.</li>
+        <li>지원 후 이력서의 내용을 수정해도 지원이력서에서는 반영되지 않습니다.</li>
+        <li>채용정보에서 홈페이지 링크를 클릭하면, 홈페이지 지원으로 자동 저장되며, 채용담당자에게는 회원님의 정보가 보이지 않습니다.</li>
+        <li>채용공고 마감 후 90일, 지원 후 120일이 지나면 지원학원은 지원이력서를 열람할 수 없습니다.</li>
+        <li>모바일에서 지원한 현황도 함께 확인할 수 있습니다.</li>
+      </ul>
+    </div>
+
     <!-- // -->
   </div>
-
 </template>
 
 <script setup></script>
